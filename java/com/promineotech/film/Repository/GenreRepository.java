64902bbb4084b2f0963c5b15d327201b3f690a22
BraxtonDHill <116772387+BraxtonDHill@users.noreply.github.com>
2023-03-25 18:05:41 -0500

    Delete java/com/promineotech/film directory

--- java/com/promineotech/film/Repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.promineotech.film.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.promineotech.film.entity.genre;
-
-public interface GenreRepository extends JpaRepository<genre, Integer> {
-}
